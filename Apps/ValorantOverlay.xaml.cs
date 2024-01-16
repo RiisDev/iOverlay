8481ed3e8203a7a61d837356f955a79dd9db0c10
Iris <39096994+RiisDev@users.noreply.github.com>
2024-01-16 12:23:36 -0700

    More Updates

--- Apps/ValorantOverlay.xaml.cs
+++ Apps/ValorantOverlay.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System.Diagnostics;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using iOverlay.Logic;
 using iOverlay.Logic.WidgetLogic;
 using RadiantConnect;
+using RadiantConnect.Methods;
 using RadiantConnect.Network.PVPEndpoints.DataTypes;
 namespace iOverlay.Apps;
 
@@ -14,7 +16,6 @@ public partial class ValorantOverlay
     private readonly Random _backgroundRandom = new(DateTime.Now.Millisecond);
 
     private string? _seasonId;
-    private string? _userId;
     private bool _firstRun;
 
     private readonly List<string> _backgroundAssets =
@@ -41,9 +42,9 @@ public partial class ValorantOverlay
     private async void Window_Loaded(object sender, RoutedEventArgs e)
     {
         _initiator = new Initiator();
-        _userId = _initiator.ExternalSystem.ClientData.UserId;
+        Debug.WriteLine("Init");
         Content? content = await _initiator.Endpoints.PvpEndpoints.FetchContentAsync();
-        _seasonId = content?.Seasons.FirstOrDefault(s => s.IsActive!.Value && s.Name.Contains("ACT"))?.ID;
+        _seasonId = content?.Seasons.FirstOrDefault(s => s.IsActive!.Value && s.Type == "act")?.ID;
 
         RunRankCheck();
 
@@ -82,22 +83,27 @@ public partial class ValorantOverlay
 
         foreach ((Match? matchInternalDetails, MatchInfo? matchDetails) in matchInfo)
         {
-            double totalHeadshots = 0;
-            double totalHits = 0;
-
-            IEnumerable<PlayerStat>? playerStats = matchDetails?.RoundResults.SelectMany(roundResult => roundResult.PlayerStats).Where(playerResults => playerResults.Subject == _userId);
-            
-            if (matchInternalDetails.RankedRatingEarned > 0)
-                totalWins++;
-
-
-            foreach (Damage damageHits in playerStats?.SelectMany(playerResults => playerResults.DamageInternal)!)
+            try
             {
-                totalHeadshots += damageHits.Headshots ?? 0;
-                totalHits += (damageHits.Bodyshots ?? 0) + (damageHits.Legshots ?? 0) + (damageHits.Headshots ?? 0);
-            }
+                double totalHeadshots = 0;
+                double totalHits = 0;
 
-            headshotPercentages.Add(Math.Round((totalHeadshots / totalHits) * 100));
+                List<PlayerStat>? playerStats = matchDetails?.RoundResults.SelectMany(roundResult => roundResult.PlayerStats).Where(playerResults => playerResults.Subject == _initiator.Client.UserId).ToList();
+
+                if (matchInternalDetails.RankedRatingEarned > 0)
+                    totalWins++;
+
+                for (int playerStatIndex = 0; playerStatIndex < playerStats?.Count; playerStatIndex++)
+                {
+                    foreach (Damage damageHits in playerStats[playerStatIndex].DamageInternal)
+                    {
+                        totalHeadshots += damageHits.Headshots ?? 0;
+                        totalHits += (damageHits.Bodyshots ?? 0) + (damageHits.Legshots ?? 0) + (damageHits.Headshots ?? 0);
+                    }
+                }
+
+                headshotPercentages.Add(Math.Round((totalHeadshots / totalHits) * 100));
+            }catch{}
         }
         
         double headshotAverage = headshotPercentages.Count == 0 ? 0.0 : headshotPercentages.Average();
@@ -107,15 +113,12 @@ public partial class ValorantOverlay
 
     private async void RunRankCheck()
     {
-        if (_userId is null) return;
-
-        CompetitiveUpdate? competitiveUpdate = await _initiator.Endpoints.PvpEndpoints.FetchCompetitveUpdatesAsync(_userId);
+        CompetitiveUpdate? competitiveUpdate = await _initiator.Endpoints.PvpEndpoints.FetchCompetitveUpdatesAsync(_initiator.Client.UserId);
 
         bool playedThisSeason = competitiveUpdate?.Matches.FirstOrDefault(x => x.SeasonID == _seasonId) != null;
         if (!playedThisSeason) return;
-
-        string currentRankName = competitiveUpdate?.Matches[0].TierAfterUpdate?.ToString().Replace("_", "") ?? "Unranked";
-        if (currentRankName.Contains("Unranked")) currentRankName = currentRankName[..7];
+        
+        string currentRankName = ValorantTables.TierToRank[competitiveUpdate?.Matches[0].TierAfterUpdate!.Value ?? 0];
 
         long rankRating = competitiveUpdate?.Matches[0].RankedRatingAfterUpdate ?? 0;
 
